51777d024fba57423c9bec1f73a8cdd5137ee7e2
qthang <quangthangdk95@gmail.com>
2024-06-30 22:09:48 +0700

    Update TF_keys.js

--- TF_keys.js
+++ TF_keys.js
@@ -11,33 +11,26 @@
 const reg1 = /^https:\/\/testflight\.apple\.com\/v3\/accounts\/(.*)\/apps$/;
 const reg2 = /^https:\/\/testflight\.apple\.com\/join\/(.*)/;
 if (reg1.test($request.url)) {
-  $persistentStore.write(null, "request_id");
-  let url = $request.url;
-  let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, "$2");
-  let session_id =
-    $request.headers["X-Session-Id"] || $request.headers["x-session-id"];
-  let session_digest =
-    $request.headers["X-Session-Digest"] ||
-    $request.headers["x-session-digest"];
-  let request_id =
-    $request.headers["X-Request-Id"] || $request.headers["x-request-id"];
-  let ua = $request.headers["User-Agent"] || $request.headers["user-agent"];
-  $persistentStore.write(key, "key");
-  $persistentStore.write(session_id, "session_id");
-  $persistentStore.write(session_digest, "session_digest");
-  $persistentStore.write(request_id, "request_id");
-  $persistentStore.write(ua, "tf_ua");
-  console.log($request.headers);
-  if ($persistentStore.read("request_id") !== null) {
-    $notification.post("TF信息获取", "信息获取成功，请关闭脚本！", "");
-  } else {
-    $notification.post(
-      "TF信息获取",
-      "信息获取失败，请打开Mitm over HTTP2开关，并重启VPN和TestFlight App！",
-      ""
-    );
-  }
-  $done({});
+    $persistentStore.write(null, 'request_id')
+    let url = $request.url
+    let key = url.replace(/(.*accounts\/)(.*)(\/apps)/, '$2')
+    let session_id = $request.headers['X-Session-Id'] || $request.headers['x-session-id']
+    let session_digest = $request.headers['X-Session-Digest'] || $request.headers['x-session-digest']
+    let request_id = $request.headers['X-Request-Id'] || $request.headers['x-request-id']
+    let ua = $request.headers['User-Agent'] || $request.headers['user-agent']
+    $persistentStore.write(key, 'key')
+    $persistentStore.write(session_id, 'session_id')
+    $persistentStore.write(session_digest, 'session_digest')
+    $persistentStore.write(request_id, 'request_id')
+    $persistentStore.write(ua, 'tf_ua')
+    console.log($request.headers)
+    if ($persistentStore.read('request_id') !== null) {
+      $notification.post('Thu thập thông tin TF', 'Thành công, vui lòng đóng tập lệnh!','')
+
+    } else {
+      $notification.post('Thu thập thông tin TF','Không thành công, vui lòng bật công tắc Mitm qua HTTP2 và khởi động lại VPN và Ứng dụng TestFlight!','')
+    }
+    $done({})
 }
 if (reg2.test($request.url)) {
   let appId = $persistentStore.read("APP_ID");
@@ -52,12 +45,8 @@ if (reg2.test($request.url)) {
     appId = arr.join(",");
   }
   $persistentStore.write(appId, "APP_ID");
-  $notification.post(
-    "TestFlight自动加入",
-    `已添加APP_ID: ${id}`,
-    `当前ID: ${appId}`
-  );
-  $done({});
+  $notification.post("TestFlight tự động tham gia", `APP_ID đã thêm: ${id}`, `ID hiện tại: ${appId}`);
+  $done({})
 }
 function unique(arr) {
   return Array.from(new Set(arr));
